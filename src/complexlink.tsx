9493a000e0a639bd31553b82276f56bb09c9ceea
jhs129 <john.h.schneider@gmail.com>
2024-11-11 16:09:46 -0500

    misc updates

--- src/complexlink.tsx
+++ src/complexlink.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { ChangeEvent, FC } from 'react';
+import React, { useState, ChangeEvent, FC } from 'react';
 
 interface ComplexLinkProps {
     value: string;
@@ -7,6 +6,12 @@ interface ComplexLinkProps {
 }
 
 const ComplexLink: React.FC<ComplexLinkProps> = (props) => {
+    const [type, setType] = useState('model');
+
+    const handleTypeChange = (e: ChangeEvent<HTMLSelectElement>) => {
+        setType(e.target.value);
+    };
+
     const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
         props.onChange(e.target.value);
     };
@@ -14,16 +19,20 @@ const ComplexLink: React.FC<ComplexLinkProps> = (props) => {
     return (
         <div style={{ display: 'grid', gridTemplateColumns: '1fr 2fr', gap: '8px', alignItems: 'center' }}>
             <label style={{ gridColumn: '1 / 2' }}>
-                Type:
+                Link Type:
             </label>
-            <select style={{ gridColumn: '2 / 3' }}>
+            <select id="type" style={{ gridColumn: '2 / 3' }} value={type} onChange={handleTypeChange}>
                 <option value="model">Model</option>
                 <option value="url">URL</option>
             </select>
-            <label style={{ gridColumn: '1 / 2' }}>
-                Input 2:
-            </label>
-            <input style={{ gridColumn: '2 / 3' }} type="text" value={props.value} onChange={handleChange} />
+            {type === 'url' && (
+                <>
+                    <label style={{ gridColumn: '1 / 2' }}>
+                        Enter Url:
+                    </label>
+                    <input id="link" style={{ gridColumn: '2 / 3' }} type="text" value={props.value} onChange={handleChange} />
+                </>
+            )}
         </div>
     );
 };

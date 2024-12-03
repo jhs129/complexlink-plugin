3e9238f5645877064588fab96791baad5137f92c
jhs129 <john.h.schneider@gmail.com>
2024-12-03 14:11:12 -0500

    not working version

--- src/complexlink.tsx
+++ src/complexlink.tsx
@@ -1,20 +1,25 @@
 import React, { useState, ChangeEvent } from 'react';
 
 interface ComplexLinkProps {
-    value: string;
-    onChange: (value: string) => void;
+    value: {
+        type: string;
+        link: string;
+    };
+    onChange: (value: { type: string; link: string }) => void;
     defaultType?: string;
 }
 
 const ComplexLink: React.FC<ComplexLinkProps> = ({ value, onChange, defaultType = 'model' }) => {
-    const [type, setType] = useState(defaultType);
+    const [type, setType] = useState(value.type || defaultType);
 
     const handleTypeChange = (e: ChangeEvent<HTMLSelectElement>) => {
-        setType(e.target.value);
+        const newType = e.target.value;
+        setType(newType);
+        onChange({ ...value, type: newType });
     };
 
-    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-        onChange(e.target.value);
+    const handleLinkChange = (e: ChangeEvent<HTMLInputElement>) => {
+        onChange({ ...value, link: e.target.value });
     };
 
     return (
@@ -27,7 +32,7 @@ const ComplexLink: React.FC<ComplexLinkProps> = ({ value, onChange, defaultType 
             {type === 'url' && (
                 <>
                     <label htmlFor="link">Enter Url:</label>
-                    <input id="link" type="text" value={value} onChange={handleChange} />
+                    <input id="link" type="text" value={value.link} onChange={handleLinkChange} />
                 </>
             )}
         </div>
